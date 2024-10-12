2e425aeb7ebd14f773aa5a12f38548c819e443ce
BuildTools <unconfigured@null.spigotmc.org>
2024-10-12 21:27:01 +0100

    Updated maintainDaytime() to trigger when an instance of the class is created

--- src/main/java/com/probreezer/multiFuse/Listeners/WorldListener.java
+++ src/main/java/com/probreezer/multiFuse/Listeners/WorldListener.java
@@ -7,20 +7,25 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.weather.ThunderChangeEvent;
 import org.bukkit.event.weather.WeatherChangeEvent;
 
+import static org.bukkit.Bukkit.getServer;
+
 public class WorldListener implements Listener {
 
     private final MultiFuse plugin;
 
     public WorldListener(MultiFuse plugin) {
         this.plugin = plugin;
+        maintainDaytime();
     }
 
-    public static void maintainDaytime() {
-        for (var world : Bukkit.getWorlds()) {
-            world.setTime(6000);
-            world.setStorm(false);
-            world.setThundering(false);
-        }
+    public void maintainDaytime() {
+        getServer().getScheduler().runTaskTimer(plugin, () -> {
+            for (var world : Bukkit.getWorlds()) {
+                world.setTime(6000);
+                world.setStorm(false);
+                world.setThundering(false);
+            }
+        }, 0L, 600L);
     }
 
     @EventHandler
