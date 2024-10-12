be180ac29b23c550ef64a134b32960d0a27938bf
BuildTools <unconfigured@null.spigotmc.org>
2024-10-12 21:22:02 +0100

    Updated to handle pause and resume commands

--- src/main/java/com/probreezer/multiFuse/Utils/CountdownManager.java
+++ src/main/java/com/probreezer/multiFuse/Utils/CountdownManager.java
@@ -17,27 +17,35 @@ public class CountdownManager {
     }
 
     public static void updateLobbyCountdown(MultiFuse plugin) {
+        if (plugin.game.state) return;
         var config = plugin.getConfig();
         var numberOfPlayers = plugin.getServer().getOnlinePlayers().size();
-        var countdownManager = plugin.countdownManager;
+        var countdownManager = plugin.game.countdownManager;
         var countdownConfig = config.getConfigurationSection("Phases.Start");
-        countdownManager.createCountdown("Start", countdownConfig.getInt("Time"), countdownConfig.getString("Title"), BarColor.valueOf(countdownConfig.getString("Colour")), plugin.game::startGame);
+        countdownManager.createCountdown("start", countdownConfig.getInt("Time"), countdownConfig.getString("Title"), BarColor.valueOf(countdownConfig.getString("Colour")), plugin.game::startGame);
 
         if (numberOfPlayers > 1) {
-            countdownManager.startCountdown("Start");
+            countdownManager.startCountdown("start");
         } else {
-            countdownManager.cancelCountdown("Start");
+            countdownManager.cancelCountdown("start");
         }
     }
 
     public void createCountdown(String id, int seconds, String title, BarColor color, Runnable onCompleteAction) {
-        var countdown = new BossBarUtils(plugin, id, seconds, title, color, onCompleteAction);
-        countdowns.put(id, countdown);
+        var countdown = countdowns.get(id);
+        if (countdown == null) {
+            countdown = new BossBarUtils(plugin, id, seconds, title, color, onCompleteAction);
+            countdowns.put(id, countdown);
+        }
+    }
+
+    public BossBarUtils getCountdown(String id) {
+        return countdowns.get(id);
     }
 
     public void startCountdown(String id) {
         var countdown = countdowns.get(id);
-        if (countdown != null) {
+        if (countdown != null && !countdown.isPaused) {
             countdown.start();
         }
     }
@@ -46,26 +54,20 @@ public class CountdownManager {
         var countdown = countdowns.get(id);
         if (countdown != null) {
             countdown.cancel();
+            countdowns.remove(id);
         }
-    }
-
-    public boolean getIsCancelled(String id) {
-        var cancelled = false;
-        var countdown = countdowns.get(id);
-        if (countdown != null) {
-            cancelled = countdown.isCancelled;
-        }
-        return cancelled;
     }
 
     public void cancelAllCountdowns() {
         var countdownList = new ArrayList<>(countdowns.values());
-        for (BossBarUtils countdown : countdownList) {
+        for (var countdown : countdownList) {
             countdown.cancel();
         }
         countdowns.clear();
     }
 
-
+    public boolean IsCountdown(String id) {
+        return countdowns.containsKey(id);
+    }
 }
 
