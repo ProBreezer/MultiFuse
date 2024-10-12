eb85c7621651f227d25dfafe56a408074753b4a8
BuildTools <unconfigured@null.spigotmc.org>
2024-10-12 21:20:18 +0100

    Added ability to get any config file

--- src/main/java/com/probreezer/multiFuse/Utils/ConfigUtils.java
+++ src/main/java/com/probreezer/multiFuse/Utils/ConfigUtils.java
@@ -1,18 +1,28 @@
 package com.probreezer.multiFuse.Utils;
 
 import com.probreezer.multiFuse.MultiFuse;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
 
 public class ConfigUtils {
     public static void createCustomConfigs(MultiFuse plugin) {
-        var configFiles = new String[]{"teams.yml", "menus.yml", "kits.yml"};
+        var configFiles = new String[]{"blocks.yml", "kits.yml", "menus.yml", "roles.yml", "shop.yml", "teams.yml"};
 
         for (String configFile : configFiles) {
             var file = new File(plugin.getDataFolder(), configFile);
             if (!file.exists()) {
+                plugin.getLogger().warning("Config file not found: " + configFile);
+                plugin.getLogger().warning("Creating " + configFile + "...");
                 plugin.saveResource(configFile, false);
             }
         }
+    }
+
+    public static YamlConfiguration getConfig(String name) {
+        var plugin = Bukkit.getPluginManager().getPlugin("MultiFuse");
+        var file = new File(plugin.getDataFolder(), name + ".yml");
+        return YamlConfiguration.loadConfiguration(file);
     }
 }
