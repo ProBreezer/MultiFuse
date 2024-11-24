eacafdb0d3a033d664729b782ab363c70044d91f
BuildTools <unconfigured@null.spigotmc.org>
2024-11-24 18:52:21 +0000

    Removed unused event listeners - Merged into Core

--- src/main/java/com/probreezer/multiFuse/Listeners/EventLoader.java
+++ src/main/java/com/probreezer/multiFuse/Listeners/EventLoader.java
@@ -5,11 +5,9 @@ import com.probreezer.multiFuse.MultiFuse;
 public class EventLoader {
     public static void registerEvents(MultiFuse plugin) {
         var pluginManager = plugin.getServer().getPluginManager();
-        pluginManager.registerEvents(new WorldListener(plugin), plugin);
         pluginManager.registerEvents(new BlockListener(plugin), plugin);
         pluginManager.registerEvents(new FuseListener(plugin), plugin);
         pluginManager.registerEvents(new PlayerListener(plugin), plugin);
         pluginManager.registerEvents(new MenuListener(plugin), plugin);
-        pluginManager.registerEvents(new ChatListener(plugin), plugin);
     }
 }
